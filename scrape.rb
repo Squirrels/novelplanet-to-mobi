53d4748097959214df9ae791b5d362c60235592d
Squirrels <chocro@gmail.com>
2018-03-11 22:12:27 -0300

    feat(chapters): now all chapters can be converted

--- scrape.rb
+++ scrape.rb
@@ -1,27 +1,45 @@
 require 'nokogiri' 
 require 'rubygems'
 require 'open-uri'
-# require "pdfkit"
-# require "prawn"
-# require "loofah"
 require "erb"
+require 'pry'
 
 # Settings
 # Disable alarm
 #Prawn::Font::AFM.hide_m17n_warning = true
 
-novel_url = "http://novelplanet.com/Novel/Re-Monster/Volume-1-Chapter-1"
-
+# Ask for the main site
+novel_url = "http://novelplanet.com/Novel/I-Was-a-Sword-When-I-Reincarnated"
+novel_title = "I-Was-a-Sword-When-I-Reincarnated"
+novel_author = "Tanaka Yu"
+base_url = "http://novelplanet.com"
 page = Nokogiri::HTML(open(novel_url))
-novel_text = page.css('#divReadContent').inner_html
-#cleaned_text = Loofah.fragment(novel_text).scrub!(:strip).text
 
-# Now apply filters
-#cleaned_text.gsub!("&#13;","\n")
-# cleaned_text.gsub!("<h3>","[title]").gsub!("</h3>","[/title]")
-# cleaned_text.gsub!("<strong>","[bold]").gsub!("</strong>","[/bold]")
+# We look for the "Chapter list" text
+chapter_list_h3 = page.xpath('//h3[contains(text(), "Chapter list")]')
+# Now get the chapters
+chapters = chapter_list_h3.first.parent.css(".rowChapter")
+novel_text = ""
+if chapters.count == 0
+	puts "No chapters :("
+else
+	# Progress
+	chapter_total = chapters.count
+	p "#{chapter_total} chapters detected!"
+	# Reverse it!
+	chapters = chapters.reverse
+	# Now, for each, do the correct processing
+	chapters.each_with_index do |chapter, index|
+			p "#{index}/#{chapter_total} \r\n"
+			# Build url
+			chapter_url = base_url + chapter['alink']
+			page = Nokogiri::HTML(open(chapter_url))
+			# Get text
+			novel_text += page.css('#divReadContent').inner_html + "< /br>"
+	end
+end
 
-# Store it in a temp html
+binding.pry
 @content = novel_text
 
 
@@ -30,27 +48,13 @@ template = File.read('./template.html.erb')
 result = ERB.new(template).result(binding)
 
 # write result to file
-File.open('filename.html', 'w+') do |f|
+File.open("#{novel_title}.html", 'w+') do |f|
   f.write result
 end
 
 
 # Convert it using Calibre
-system("ebook-convert filename.html my-project.mobi \
-    --output-profile kindle_dx --no-inline-toc \
-    --title 'Your Book Title' --publisher 'Your Name' \
-    --language en --authors 'Your Author Name'")
-
-# Prawn::Document.generate("hello.pdf") do
-#   #text novel_text
-#   # Line by line, apply style filters
-#   novel_text.each_line do |line|
-# 	   if line.include? "<h3>"
-# 	   	text Loofah.fragment(line).scrub!(:strip).text.gsub!("&#13;","\n"), size: 20
-# 	   elsif line.include? "<strong>"
-# 	   	text Loofah.fragment(line).scrub!(:strip).text.gsub!("&#13;","\n"), style: :bold
-# 	   else
-# 	   	text Loofah.fragment(line).scrub!(:strip).text.gsub!("&#13;","\n")
-# 	   end
-# 	end
-# end
+# system("ebook-convert #{novel_title}.html #{novel_title}.mobi \
+#     --output-profile kindle_dx --no-inline-toc \
+#     --title '#{novel_title}' --publisher 'Squirrel' \
+#     --language en --authors 'Your Author Name'")
